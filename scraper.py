5b85c942014db7f046016ee7af02b711ca1c2643
CariLess <90604545+CariLess@users.noreply.github.com>
2023-01-19 18:50:56 -0600

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+START_URL = "https://exoplanets.nasa.gov/discovery/exoplanet-catalog/"
+
+browser = webdriver.Chrome("/Users/cariless/Downloads/C127/chromedriver.exe")
+browser.get(START_URL)
+
+time.sleep(10)
+
+planets_data = []
+
+def scrape():
+    for i in range(0,10):
+        print(f'Extrayendo página {i+1}...')
+
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+
+        for ul_tag in soup.find_all("ul", attrs = {"class", "exoplantet"}):
+            li_tags = ul_tag.find_all("li")
+
+            temp_list = []
+
+            for index, li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planets_data.append(temp_list)
+            print(planets_data[1])
+
+        # Encontrar todos los elementos en la página y hacer clic para desplazarse a la siguiente
+        browser.find_element(by=By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+
+
+scrape()
+
